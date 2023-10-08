e76f2cfc192962bc9f2a66e7e3034d68a73a6402
Maorid <12201510@bsi.ac.id>
2023-10-08 16:46:22 +0700

    [ADD] build gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-parcelize")
+    id ("kotlin-kapt")
 }
 
 android {
     namespace = "com.movie.trvn"
     compileSdk = 33
+
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.movie.trvn"
@@ -36,11 +42,38 @@ android {
 }
 
 dependencies {
+    val roomVersion = "2.5.2"
+    val lifecycleVersion = "2.6.2"
+    val retrofitVersion = "2.9.0"
+    val okhttp3Version = "4.11.0"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    //core
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.8.0")
+
+    //layout
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation  ("com.google.code.gson:gson:$retrofitVersion")
+
+    //okhttp
+    implementation ("com.squareup.okhttp3:okhttp:$okhttp3Version")
+    implementation ("com.squareup.okhttp3:logging-interceptor:$okhttp3Version")
+
+    // ViewModel & LiveData
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
